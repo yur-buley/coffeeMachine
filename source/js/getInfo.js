12f7f3c7cbfedf68542a105eff079aea6736511e
yur-buley <yurgan.crazy@gmail.com>
2018-07-01 20:13:00 +0300

    add getInfo

---
+++ source/js/getInfo.js
@@ -0,0 +1,3 @@
+function getInfo() {
+  $('.info').html('Вода: ' + waterVolume + ' / ' + maxWater + '</br>Молоко: ' + milkVolume + ' / ' + maxMilk + '</br>Зёрна: ' + beansVolume + ' / ' + maxBeans + '</br>Сахар: ' + sugarVolume + ' / ' + maxSugar)
+}
