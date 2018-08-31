80b3df0f28982c423f693090a2369b6321e6b374
yur-buley <yurgan.crazy@gmail.com>
2018-08-31 23:11:06 +0300

    add funcs

--- source/js/machine.js
+++ source/js/machine.js
@@ -1,92 +1,152 @@
 var panas = new CoffeeMachine(4000, 1500, 300, 150, 100)
+var power
+  var milk = 70
+  var waterCap = 100
+  var waterEsp = 80
 
-function CoffeeMachine (power, maxWater, maxMilk, maxBeans, maxSugar) {
+function CoffeeMachine (pwr, maxWater, maxMilk, maxBeans, maxSugar) {
   $('.info').text('АППАРАТ ВКЛЮЧЕН !')
 
   var waterVolume = 0
   var milkVolume = 0
   var beansVolume = 0
   var sugarVolume = 0
-  var waterCap = 100
-  var waterEsp = 80
-  var milk = 70
+
   var beans = 8
   var sugar = 6
+
+  power = pwr
 
   this.fullFill = function() {
     waterVolume = maxWater
     milkVolume = maxMilk
     beansVolume = maxBeans
     sugarVolume = maxSugar
-    console.log(waterVolume, milkVolume, beansVolume, sugarVolume)
     getInfo()
+    addIngrs()
   }
 
   this.fillWater = function () {
     newWater = + $('#add_water').val()
     if ((newWater + waterVolume) < waterEsp) {
-      $('.info').html("Неверное количество воды!</br>Добавьте от " + (waterEsp - waterVolume) + " до " + (maxWater - waterVolume) + " мл воды.")
-      return
+      $('.err').html("Неверное количество воды!</br>Добавьте от " + (waterEsp - waterVolume) + " до " + (maxWater - waterVolume) + " мл воды.")
     } else if ((newWater + waterVolume) > maxWater) {
-      $('.info').html("Неверное количество воды!</br>Добавьте не более " + (maxWater - waterVolume) + " мл воды.")
-      return
+      $('.err').html("Неверное количество воды!</br>Добавьте не более " + (maxWater - waterVolume) + " мл воды.")
     } else {
       waterVolume += newWater
       getInfo()
     }
-    console.log(waterVolume, milkVolume, beansVolume, sugarVolume)
   }
 
   this.fillMilk = function () {
     newMilk = + $('#add_milk').val()
-      if ((newMilk + milkVolume) < milk) {
-        $('.info').html("Неверное количество молока!</br>Добавьте от " + (milk - milkVolume) + " до " + (maxMilk - milkVolume) + " мл молока.")
-        return
-      } else if ((newMilk + milkVolume) > maxMilk) {
-        $('.info').html("Неверное количество молока!</br>Добавьте не более " + (maxMilk - milkVolume) + " мл молока.")
-        return
+    if ((newMilk + milkVolume) < milk) {
+      $('.err').html("Неверное количество молока!</br>Добавьте от " + (milk - milkVolume) + " до " + (maxMilk - milkVolume) + " мл молока.")
+    } else if ((newMilk + milkVolume) > maxMilk) {
+      $('.err').html("Неверное количество молока!</br>Добавьте не более " + (maxMilk - milkVolume) + " мл молока.")
+    } else {
+      milkVolume += newMilk
+      getInfo()
+    }
+  }
+
+    this.fillBeans = function () {
+      newBeans = + $('#add_beans').val()
+      if ((newBeans + beansVolume) < beans) {
+        $('.err').html("Неверное количество кофейных зёрен!</br>Добавьте от " + (beans - beansVolume) + " до " + (maxBeans - beansVolume) + " г кофейных зёрен.")
+      } else if ((newBeans + beansVolume) > maxBeans) {
+        $('.err').html("Неверное количество кофейных зёрен!</br>Добавьте не более " + (maxBeans - beansVolume) + " г кофейных зёрен.")
       } else {
-        milkVolume += newMilk
+        beansVolume += newBeans
         getInfo()
       }
     }
 
-    this.fillBeans = function () {
-      newBeans = + $('#add_beans').val()
-        if ((newBeans + beansVolume) < beans) {
-          $('.info').html("Неверное количество кофейных зёрен!</br>Добавьте от " + (beans - beansVolume) + " до " + (maxBeans - beansVolume) + " г кофейных зёрен.")
-          return
-        } else if ((newBeans + beansVolume) > maxBeans) {
-          $('.info').html("Неверное количество кофейных зёрен!</br>Добавьте не более " + (maxBeans - beansVolume) + " г кофейных зёрен.")
-          return
-        } else {
-          beansVolume += newBeans
-          getInfo()
-        }
-    }
-
     this.fillSugar = function () {
       newSugar = + $('#add_sugar').val()
-        if ((newSugar + sugarVolume) < sugar) {
-          $('.info').html("Неверное количество сахара!</br>Добавьте от " + (sugar - sugarVolume) + " до " + (maxSugar - sugarVolume) + " г сахара.")
-          return
-        } else if ((newSugar + sugarVolume) > maxSugar) {
-          $('.info').html("Неверное количество сахара!</br>Добавьте не более " + (maxSugar - sugarVolume) + " г сахара.")
-          return
-        } else {
-          sugarVolume += newSugar
-          getInfo()
-        }
+      if ((newSugar + sugarVolume) < sugar) {
+        $('.err').html("Неверное количество сахара!</br>Добавьте от " + (sugar - sugarVolume) + " до " + (maxSugar - sugarVolume) + " г сахара.")
+      } else if ((newSugar + sugarVolume) > maxSugar) {
+        $('.err').html("Неверное количество сахара!</br>Добавьте не более " + (maxSugar - sugarVolume) + " г сахара.")
+      } else {
+        sugarVolume += newSugar
+        getInfo()
+      }
+    }
+
+    this.makeCoffeeCap = function () {
+      if (waterVolume < waterCap) {
+        $('.err').text ("Недостаточно воды для приготовления напитка.")
+        return
+      } else if (milkVolume < milk) {
+        $('.err').text ("Недостаточно молока для приготовления напитка.")
+        return
+      } else if (beansVolume < beans) {
+        $('.err').text ("Недостаточно кофейных зёрен для приготовления напитка.")
+        return
+      } else if (sugarVolume < sugar) {
+        $('.err').text ("Недостаточно сахара для приготовления напитка.")
+        return
+      } else {
+        var time = getHeatingMilk() + getHeatingCap()
+        $('.info').text ("Ждите " + Math.ceil(time) + " с до окончания приготовления напитка.")
+        $('#cap').attr('disabled', true)
+        $('#esp').attr('disabled', true)
+        $('#milk').attr('disabled', true)
+        $('#water').attr('disabled', true)
+        $('#add').attr('disabled', true)
+      }
+
+      waterVolume -= waterCap
+      milkVolume -= milk
+      beansVolume -= beans
+      sugarVolume -= sugar
+
+      timerId = setTimeout (function () {
+        $('.info').text ("Ваш капучино готов!")
+        $('#cap').attr('disabled', false)
+        $('#esp').attr('disabled', false)
+        $('#milk').attr('disabled', false)
+        $('#water').attr('disabled', false)
+        $('#add').attr('disabled', false)
+      }, time*1000)
+    }
+
+    this.makeCoffeeEsp = function () {
+      if (waterVolume < waterEsp) {
+        $('.err').text ("Недостаточно воды для приготовления напитка.")
+        return
+      } else if (beansVolume < beans) {
+        $('.err').text ("Недостаточно кофейных зёрен для приготовления напитка.")
+        return
+      } else if (sugarVolume < sugar) {
+        $('.err').text ("Недостаточно сахара для приготовления напитка.")
+        return
+      } else {
+        var time = getHeatingEsp()
+        $('.info').text ("Ждите " + Math.ceil(time) + " с до окончания приготовления напитка.")
+        $('#cap').attr('disabled', true)
+        $('#esp').attr('disabled', true)
+        $('#milk').attr('disabled', true)
+        $('#water').attr('disabled', true)
+        $('#add').attr('disabled', true)
+      }
+
+      waterVolume -= waterEsp
+      beansVolume -= beans
+      sugarVolume -= sugar
+
+      timerId = setTimeout (function () {
+        $('.info').text ("Ваш эспрессо готов!")
+        $('#cap').attr('disabled', false)
+        $('#esp').attr('disabled', false)
+        $('#milk').attr('disabled', false)
+        $('#water').attr('disabled', false)
+        $('#add').attr('disabled', false)
+      }, time*1000)
     }
 
   function getInfo() {
-  $('.info').html('Вода: ' + waterVolume + ' / ' + maxWater + '</br>Молоко: ' + milkVolume + ' / ' + maxMilk + '</br>Зёрна: ' + beansVolume + ' / ' + maxBeans + '</br>Сахар: ' + sugarVolume + ' / ' + maxSugar)
+    $('.info').html('Вода: ' + waterVolume + ' / ' + maxWater + '</br>Молоко: ' + milkVolume + ' / ' + maxMilk + '</br>Зёрна: ' + beansVolume + ' / ' + maxBeans + '</br>Сахар: ' + sugarVolume + ' / ' + maxSugar)
+  }
 }
-}
-
-
-
-
-
-// cd /d d:\Learning_Prog\coffeeMachine
-// gulp watch
