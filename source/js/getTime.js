1d7fc2be81d8209db37a789d60933ec6860a94cf
yur-buley <yur-buley@github.com>
2018-09-01 03:40:16 +0300

    modified functions

--- source/js/getTime.js
+++ source/js/getTime.js
@@ -1,11 +1,7 @@
-function getHeatingEsp () {
-  return waterEsp / 1000 * 70 * 4200 / power
+function getHeatingWater (w) {
+  return w / 1000 * 70 * 4200 / power
 }
 
-function getHeatingCap () {
-  return waterCap / 1000 * 70 * 4200 / power
-}
-
-function getHeatingMilk () {
-  return milk / 1000 * 60 * 3900 / power
+function getHeatingMilk (m) {
+  return m / 1000 * 60 * 3900 / power
 }
